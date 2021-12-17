12a23cc21306b52612d1f298dbf4b53331a4b74a
Leonardo Lopes <leolopesds1999@gmail.com>
2021-12-17 20:25:03 -0300

    adição est e mun data

--- main.R
+++ main.R
@@ -3,23 +3,33 @@
 if (!requireNamespace("pacman", quietly = TRUE)) {
   install.packages("pacman")
 }
-pacman::p_load(readxl,geobr,tidyverse,sf,rChoiceDialogs)
+pacman::p_load(readxl,geobr,tidyverse,sf,rChoiceDialogs,feather)
 
 
-
+#
 dados <- read_excel("data/eleicaoPresCidade.xlsx",sheet = "Planilha3")
 
-est <- read_state(code_state = "all", year=2010)
-mun <- read_municipality(code_muni="all", year=2010)
+if (!file.exists("data/est.Rdata")){
+  est <- read_state(code_state = "all", year=2010)
+  save(est, file = "data/est.Rdata")
+} else {
+  load("data/est.Rdata")
+}
+if (!file.exists("data/mun.Rdata")){
+  mun <- read_municipality(code_muni="all", year=2010)
+  save(mun, file = "data/mun.Rdata")
+} else {
+  load("data/mun.Rdata")
+}
 
-mun <- left_join(mun, dados, by= c("code_muni" = "id_municipio"))
+#
+mun2 <- left_join(mun, dados, by= c("code_muni" = "id_municipio"))
 
-mun2 <-mun %>% mutate(perda = PT_2014-PT_2018)
+#mun2 <-mun %>% mutate(perda = PT_2014-PT_2018)
 
 
 choosepath <- function() {
-  testpath<-print(interactive())
-  if (testpath == TRUE) {
+  if (interactive()) {
   
     path <- rchoose.dir() 
   } else {
@@ -33,7 +43,7 @@ path <- choosepath()
 
 ## Plot do mapa 2014
 ggplot()+ 
-  geom_sf(data=mun, aes(fill= PercPT2014),color='transparent')+
+  geom_sf(data=mun2, aes(fill= PercPT2014),color='transparent')+
   geom_sf(fill='transparent',color='#F6F9FF',data=est, size=.5)+
   scale_fill_gradient2(low = "#0C4196", mid = "white", high = "#E62339", midpoint = .5)+
   theme_void()+
@@ -56,7 +66,7 @@ ggsave(
 
 ## plot do mapa 2018
 ggplot()+ 
-  geom_sf(data=mun, aes(fill= PercPT2018),color='transparent')+
+  geom_sf(data=mun2, aes(fill= PercPT2018),color='transparent')+
   geom_sf(fill='transparent',color='#F6F9FF',data=est, size=.5)+
   scale_fill_gradient2(low = "#297D1C", mid = "white", high = "#E62339", midpoint = .5)+
   theme_void()+
