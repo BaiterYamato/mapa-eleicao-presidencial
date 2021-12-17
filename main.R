d4c85a1f1f6cf061296251f955a2c65f14e6a7b9
Leonardo Lopes <leolopesds1999@gmail.com>
2021-12-17 16:55:32 -0300

    adicionado export
    criado função de export dos mapas

--- main.R
+++ main.R
@@ -7,7 +7,7 @@ pacman::p_load(readxl,geobr,tidyverse,sf)
 
 
 
-dados <- read_excel("C:/Users/Baiter/Documents/AQOON/eleicaoPresCidade.xlsx",sheet = "Planilha3")
+dados <- read_excel("data/eleicaoPresCidade.xlsx",sheet = "Planilha3")
 
 est <- read_state(code_state = "all", year=2010)
 mun <- read_municipality(code_muni="all", year=2010)
@@ -26,6 +26,20 @@ ggplot()+
   theme(panel.background = element_rect(fill = '#F6F9FF', colour = '#F6F9FF'))+
   theme(legend.position = 'none',legend.title = element_blank(), )
 
+ggsave(
+  "mapa2014.png",
+  plot = last_plot(),
+  device = "png",
+  path = "export",
+  scale = 1,
+  width = NA,
+  height = NA,
+  units = c("in", "cm", "mm", "px"),
+  dpi = 300,
+  limitsize = TRUE,
+  bg = NULL
+)
+
 ## plot do mapa 2018
 ggplot()+ 
   geom_sf(data=mun, aes(fill= PercPT2018),color='transparent')+
@@ -35,6 +49,20 @@ ggplot()+
   theme(panel.background = element_rect(fill = '#F6F9FF', colour = '#F6F9FF'))+
   theme(legend.position = 'none',legend.title = element_blank(), )
 
+ggsave(
+  "mapa2018.png",
+  plot = last_plot(),
+  device = "png",
+  path = "export",
+  scale = 1,
+  width = NA,
+  height = NA,
+  units = c("in", "cm", "mm", "px"),
+  dpi = 300,
+  limitsize = TRUE,
+  bg = NULL
+)
+
 ##
-ggplot() + geom_sf(data=mun2, aes(fill= perda),color = 'transparent')+
-  scale_fill_gradient2(low = "#297D1C", mid = "white", high = "#E62339", midpoint = 0)
+#ggplot() + geom_sf(data=mun2, aes(fill= perda),color = 'transparent')+
+#  scale_fill_gradient2(low = "#297D1C", mid = "white", high = "#E62339", midpoint = 0)
