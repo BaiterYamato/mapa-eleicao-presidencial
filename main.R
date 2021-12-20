0d6e4806e1a2c1066895da620699717c2ccb3caa
Leonardo Lopes <leolopesds1999@gmail.com>
2021-12-20 16:11:45 -0300

    refatoramento e melhoria do plot perda de votos
    também ajustado alguns códigos

--- main.R
+++ main.R
@@ -25,12 +25,21 @@ if (!file.exists("data/mun.Rdata")){
 
 
 ## transformacao dados
-mun<-subset(mun,code_muni != 4300001 )
-mun<-subset(mun,code_muni != 4300002 )
+{
+#
+  mun<-subset(mun,code_muni != 4300001 )
+  mun<-subset(mun,code_muni != 4300002 )
 
-mun <- left_join(mun, dados, by= c("code_muni" = "id_municipio"))
-mun <- mun %>% mutate(perda = PT_2014-PT_2018)
+#juntar dados de municipio com os dados de eleicao
+  mun <- left_join(mun, dados, by= c("code_muni" = "id_municipio"))
 
+#criar perda de votos no pt
+  mun <- mun %>% mutate(perda = PT_2014-PT_2018)
+
+# criar intervalos para a perda de votos
+  breaks_qt <- classIntervals(mun$perda, n = 5, style = "quantile")
+  mun <- mutate(mun, perda_cat = cut(perda, breaks_qt$brks)) 
+}
 
 ## funcao path para export
 choosepath <- function() {
@@ -49,7 +58,7 @@ path <- choosepath()
 
 ## Plot do mapa 2014
 ggplot()+ 
-  geom_sf(data=mun2, aes(fill= PercPT2014),color='transparent')+
+  geom_sf(data=mun, aes(fill= PercPT2014),color='transparent')+
   geom_sf(fill='transparent',color='#F6F9FF',data=est, size=.5)+
   scale_fill_gradient2(low = "#0C4196", mid = "white", high = "#E62339", midpoint = .5)+
   theme_void()+
@@ -73,7 +82,7 @@ ggsave(
 
 ## plot do mapa 2018
 ggplot()+ 
-  geom_sf(data=mun2, aes(fill= PercPT2018),color='transparent')+
+  geom_sf(data=mun, aes(fill= PercPT2018),color='transparent')+
   geom_sf(fill='transparent',color='#F6F9FF',data=est, size=.5)+
   scale_fill_gradient2(low = "#297D1C", mid = "white", high = "#E62339", midpoint = .5)+
   theme_void()+
@@ -96,16 +105,28 @@ ggsave(
 
 
 ## plot mapa perda de votos do PT
-
-#ggplot() + geom_sf(data=mun2, aes(fill= perda),color = 'transparent')+
-#  scale_fill_gradient2(low = "#297D1C", mid = "white", high = "#E62339", midpoint = 0)
-
-breaks_qt <- classIntervals(mun2$perda, n = 5, style = "quantile")
-breaks_qt
-mun2 <- mutate(mun2, perda_cat = cut(perda, breaks_qt$brks)) 
-
 ggplot()+ 
-  geom_sf(data=mun2,aes(fill=perda_cat),color='transparent')+
-  scale_fill_brewer(palette = "RdYlGn")+
-  geom_sf(fill='transparent',color='#F6F9FF',data=est, size=.5)+
-  theme_void()
+  
+  geom_sf(
+    data = mun,
+    aes(fill = perda_cat),
+    color ='transparent')+
+  
+  geom_sf(
+    data=est,
+    fill='transparent',
+    color='#F6F9FF',
+    size=.5)+
+
+  labs(fill = "Diferença de votos em relação a eleição anterior")+
+  
+  scale_fill_manual(
+    na.value = 'transparent',
+    values = c("#FF4751", "#FF9A85", "#FFF0BD", "#B6E296", "#5DCE6C", "#00abff"),
+    labels = c("Aumento Para o PT significativo", "Aumento médio para o PT", "Neutro", "Perda média para o PT", "Perda para o PT significativa", "")
+    )+
+  
+  theme_void()+
+  theme(panel.background = element_rect(fill = '#F6F9FF', colour = '#F6F9FF'))
+
+
